f93693a351983e97f8b8c583bf31e269bd5b859c
MoeinFirouzi <firouzi.m.moein@gmail.com>
2022-08-18 01:28:59 +0430

    Make player app model fields nullable

--- core/player/models.py
+++ core/player/models.py
@@ -10,21 +10,23 @@ class Music(models.Model):
         ("Too Positive", "Too Positive"),
     )
 
-    title = models.CharField(max_length=250, blank=True)
+    title = models.CharField(max_length=250, blank=True, null=True)
     artist = models.ForeignKey(
-        'Artist', on_delete=models.CASCADE, related_name="musics", blank=True)
+        'Artist', on_delete=models.CASCADE, related_name="musics", blank=True, null=True)
     album = models.ForeignKey(
-        'Album', on_delete=models.CASCADE, related_name="musics", blank=True)
-    genre = models.CharField(max_length=250, blank=True)
-    energy = models.CharField(choices=CHOICES, blank=True, max_length=50)
-    valence = models.CharField(choices=CHOICES, blank=True, max_length=50)
-    song_image = models.ImageField(upload_to='music/images/', blank=True)
-    year = models.PositiveSmallIntegerField(blank=True)
-    address = models.FileField(upload_to='music/files/', blank=True)
+        'Album', on_delete=models.CASCADE, related_name="musics", blank=True, null=True)
+    genre = models.CharField(max_length=250, blank=True, null=True)
+    energy = models.CharField(choices=CHOICES, blank=True, max_length=50, null=True)
+    valence = models.CharField(choices=CHOICES, blank=True, max_length=50, null=True)
+    song_image = models.ImageField(upload_to='music/images/', blank=True, null=True)
+    year = models.PositiveSmallIntegerField(blank=True, null=True)
+    address = models.FileField(upload_to='music/files/', blank=True, null=True)
 
     def save(self, *args, **kwargs):
-        self.title = self.title.lower()
-        self.genre = self.genre.lower()
+        if self.title:
+            self.title = self.title.lower()
+        if self.genre:
+            self.genre = self.genre.lower()
         return super(Music, self).save(*args, **kwargs)
 
     def __str__(self) -> str:
@@ -33,10 +35,11 @@ class Music(models.Model):
 
 class Artist(models.Model):
     name = models.CharField(max_length=250, unique=True)
-    artist_image = models.ImageField(upload_to='artist/images/', blank=True)
+    artist_image = models.ImageField(upload_to='artist/images/', blank=True, null=True)
 
     def save(self, *args, **kwargs):
-        self.name = self.name.lower()
+        if self.name:
+            self.name = self.name.lower()
         return super(Artist, self).save(*args, **kwargs)
 
     def __str__(self) -> str:
@@ -45,10 +48,11 @@ class Artist(models.Model):
 
 class Album(models.Model):
     name = models.CharField(max_length=250, unique=True)
-    album_image = models.ImageField(upload_to='album/images/', blank=True)
+    album_image = models.ImageField(upload_to='album/images/', blank=True, null=True)
 
     def save(self, *args, **kwargs):
-        self.name = self.name.lower()
+        if self.name:
+            self.name = self.name.lower()
         return super(Album, self).save(*args, **kwargs)
 
     def __str__(self) -> str:
