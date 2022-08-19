373afc976601a7a80a678355129eafa551437c7a
MoeinFirouzi <firouzi.m.moein@gmail.com>
2022-08-19 13:00:40 +0430

    Add 'GetAlbumList' API view to get albums list

--- core/player/api/views.py
+++ core/player/api/views.py
@@ -1,8 +1,9 @@
 from rest_framework import generics
 from rest_framework import status
 from rest_framework.response import Response
-from .serializers import UploadMusicSerializer, MusicListSerializer, ArtistSerializer
-from ..models import Music, Artist
+from .serializers import (AlbumSerializer, UploadMusicSerializer, MusicListSerializer,
+                          ArtistSerializer, Album)
+from ..models import Music, Artist, Album
 
 
 class UploadMusic(generics.GenericAPIView):
@@ -39,7 +40,6 @@ class GetMusicDataList(generics.ListAPIView):
         return queryset
 
 
-
 class GetArtistList(generics.ListAPIView):
     """
     Return list of Artists in Database.
@@ -56,5 +56,25 @@ class GetArtistList(generics.ListAPIView):
 
         else:
             queryset = Artist.objects.all()
+
+        return queryset
+
+
+class GetAlbumList(generics.ListAPIView):
+    """
+    Return list of Albums in Database.
+    If 'id' parameter exists, then it will return a list of objects
+    that their id are in 'id' parameter.
+    """
+    serializer_class = AlbumSerializer
+
+    def get_queryset(self):
+        ids = self.request.GET.get('id')
+        if ids:
+            id_list = list(map(int, ids.split(',')))
+            queryset = Album.objects.filter(id__in=id_list)
+
+        else:
+            queryset = Album.objects.all()
 
         return queryset
--- core/player/urls.py
+++ core/player/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .api.views import UploadMusic, GetMusicDataList, GetArtistList
-
+from .api.views import (UploadMusic, GetMusicDataList, GetArtistList,
+                        GetAlbumList)
 urlpatterns = [
     path('upload-music/', UploadMusic.as_view(), name="upload_music"),
     path('music/list/', GetMusicDataList.as_view(), name="music_list"),
     path('artist/list/', GetArtistList.as_view(), name="artist_list"),
+    path('album/list/', GetAlbumList.as_view(), name="album_list"),
 ]
