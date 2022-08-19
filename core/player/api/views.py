f5798b8182e832be3f1d86f6e7fc37712af77744
MoeinFirouzi <firouzi.m.moein@gmail.com>
2022-08-19 11:49:08 +0430

    Change 'GetMusicDataList' to get list of musics, by their id

--- core/player/api/views.py
+++ core/player/api/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework import status
 from rest_framework.response import Response
@@ -12,12 +13,28 @@ class UploadMusic(generics.GenericAPIView):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            data = {'message': "Music has uploaded successfully", 'id': serializer.data.get('id')}
+            data = {'message': "Music has uploaded successfully",
+                    'id': serializer.data.get('id')}
             return Response(data, status=status.HTTP_200_OK)
         else:
             return Response({'message': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class GetMusicDataList(generics.ListAPIView):
+    """
+    Return list of Musics in Database.
+    If 'id' parameter exists, then it will return list of objects
+    that their id are in 'id' parameter.
+    """
     serializer_class = MusicListSerializer
-    queryset = Music.objects.all()
+
+    def get_queryset(self):
+        ids = self.request.GET.get('id')
+        if ids:
+            id_list = list(map(int, ids.split(',')))
+            queryset = Music.objects.filter(id__in=id_list)
+
+        else:
+            queryset = Music.objects.all()
+
+        return queryset
