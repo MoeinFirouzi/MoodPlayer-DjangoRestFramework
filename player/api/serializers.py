84da5bb7733aeb37a919bf6c50cb5cd578929bb8
MoeinFirouzi <firouzi.m.moein@gmail.com>
2022-09-09 23:07:44 +0430

    Change to_representation method in MusicSerializer, ArtistSerializer & AlbumSerializer

--- player/api/serializers.py
+++ player/api/serializers.py
@@ -2,10 +2,22 @@ from rest_framework import serializers
 from ..models import Music, Album, Artist
 from core.settings import PORT, BASE_URL
 
+
 class AlbumSerializer(serializers.ModelSerializer):
     class Meta:
         model = Album
         fields = "__all__"
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        base_url = BASE_URL
+        port = PORT
+        if port:
+            base_url = f"{base_url}:{port}"
+
+        artist_image_url = instance.album_image.url
+        representation['album_image'] = f"http://{base_url}{artist_image_url}"
+        return representation
 
 
 class ArtistSerializer(serializers.ModelSerializer):
@@ -13,20 +25,25 @@ class ArtistSerializer(serializers.ModelSerializer):
         model = Artist
         fields = "__all__"
 
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        base_url = BASE_URL
+        port = PORT
+        if port:
+            base_url = f"{base_url}:{port}"
+
+        artist_image_url = instance.artist_image.url
+        representation['artist_image'] = f"http://{base_url}{artist_image_url}"
+        return representation
+
 
 class MusicSerializer(serializers.ModelSerializer):
-
-    absolute_url = serializers.SerializerMethodField()
-
     artist = serializers.CharField()
     artist_id = serializers.ReadOnlyField(source="get_artist_id")
     artist_image = serializers.FileField(required=False)
 
     # Refers to "get_artist_absolute_url" in Music Model
     artist_url = serializers.ReadOnlyField(source="get_artist_absolute_url")
-
-    # Refers to "get_artist_absolute_url" in MusicSerializer
-    artist_absolute_url = serializers.SerializerMethodField()
 
     album = serializers.CharField()
     album_id = serializers.ReadOnlyField(source="get_album_id")
@@ -35,81 +52,31 @@ class MusicSerializer(serializers.ModelSerializer):
     # Refers to "get_album_absolute_url" in Music Model
     album_url = serializers.ReadOnlyField(source="get_album_absolute_url")
 
-    # Refers to "get_album_absolute_url" in MusicSerializer
-    album_absolute_url = serializers.SerializerMethodField()
-    
-    address = serializers.SerializerMethodField()
-    song_image = serializers.SerializerMethodField()
-    
     class Meta:
         model = Music
         fields = "__all__"
 
-    def get_absolute_url(self, obj):
-        """
-        Returns Music instance absolute url
-        """
-        request = self.context.get("request")
-        if request:
-            obj_url = obj.get_absolute_url()
-            base_url = BASE_URL
-            port = PORT
-            if port:
-                base_url = f"{base_url}:{port}"
-            return f"http://{base_url}{obj_url}"
-        else:
-            return None
-
-    def get_album_absolute_url(self, obj):
-        """
-        Returns related album to music instance absolute url
-        """
-        request = self.context.get("request")
-        if request:
-            obj_url = obj.album.get_absolute_url()
-            base_url = BASE_URL
-            port = PORT
-            if port:
-                base_url = f"{base_url}:{port}"
-            return f"http://{base_url}{obj_url}"
-        else:
-            return None
-
-    def get_artist_absolute_url(self, obj):
-        """
-        Returns related artist to music instance absolute url
-        """
-        request = self.context.get("request")
-        if request:
-            obj_url = obj.artist.get_absolute_url()
-            base_url = BASE_URL
-            port = PORT
-            if port:
-                base_url = f"{base_url}:{port}"
-            return f"http://{base_url}{obj_url}"
-        else:
-            return None
-
-    def get_address(self, obj):
-        obj_url = obj.address.path
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
         base_url = BASE_URL
         port = PORT
         if port:
             base_url = f"{base_url}:{port}"
-            
-        return f"http://{base_url}{obj_url}"
-        
-        
-    def get_song_image(self, obj):
-        obj_url = obj.song_image.path
-        base_url = BASE_URL
-        port = PORT
-        if port:
-            base_url = f"{base_url}:{port}"
-            
-        return f"http://{base_url}{obj_url}"
-        
-        
+
+        file_url = instance.address.url
+        song_image_url = instance.song_image.url
+        absolute_url = instance.get_absolute_url()
+        album_absolute_url = instance.album.get_absolute_url()
+        artist_absolute_url = instance.artist.get_absolute_url()
+
+        representation['address'] = f"http://{base_url}{file_url}"
+        representation['song_image'] = f"http://{base_url}{song_image_url}"
+        representation['absolute_url'] = f"http://{base_url}{absolute_url}"
+        representation['album_absolute_url'] = f"http://{base_url}{album_absolute_url}"
+        representation['artist_absolute_url'] = f"http://{base_url}{artist_absolute_url}"
+
+        return representation
+
     def create(self, validated_data):
         artist_instance, created = Artist.objects.get_or_create(
             name=validated_data["artist"]
