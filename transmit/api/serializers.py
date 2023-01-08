2baa8ac6efaa1642bccd6d2d1398c8adcc6f5e6f
MoeinFirouzi <firouzi.m.moein@gmail.com>
2023-01-08 12:01:01 +0330

    Change user_id field in SensorStateSerializer and MusicStateSerializer

--- transmit/api/serializers.py
+++ transmit/api/serializers.py
@@ -3,27 +3,18 @@ from ..models import MusicState, SensorState, Session
 
 
 class MusicStateSerializer(serializers.ModelSerializer):
-    user_id = serializers.SerializerMethodField()
 
     class Meta:
 
         model = MusicState
         fields = "__all__"
 
-    def get_user_id(self, obj):
-        return obj.get_session_user_id()
-
 
 class SensorStateSerializer(serializers.ModelSerializer):
-    user_id = serializers.SerializerMethodField()
-
     class Meta:
 
         model = SensorState
         fields = "__all__"
-
-    def get_user_id(self, obj):
-        return obj.get_session_user_id()
 
 
 class SessionSerializer(serializers.ModelSerializer):
