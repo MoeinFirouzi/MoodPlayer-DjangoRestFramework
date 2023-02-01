087c43339b5e4cf14574d7e5deab8bfc61c3fd79
MoeinFirouzi <firouzi.m.moein@gmail.com>
2023-02-01 23:18:58 +0330

    Create recommender api

---
+++ recommender/api/views.py
@@ -0,0 +1,4 @@
+from rest_framework import generics
+
+class RecommendMusic(generics.GenericAPIView):
+    pass
--- recommender/urls.py
+++ recommender/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from recommender.api.view import RecommendMusic
+from recommender.api.views import RecommendMusic
 
 urlpatterns = [
     path("recommend/", RecommendMusic.as_view(), name="recommend_music"),
